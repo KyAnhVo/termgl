5ec130d35ad8710fa76f50bd8a12317d8a070cb5
Ky Anh Vo <tommyvo0406@gmail.com>
2026-04-30 18:18:49 -0500

    added papers

---
+++ PAPERS.md
@@ -0,0 +1,12 @@
+# Papers that some algorithms are based on
+
+## Surface Simplification Using Quadric Error Metrics
+* Link: https://mgarland.org/files/papers/quadrics.pdf
+* Idea: 
+  * QEM simplification algorithm
+  * Extend the space to include uv coordinate (x, y, z, u, v) in R5
+
+## Adaptive Mesh Simplification Using Vertex Clustering with Topology Preserving 
+* Link: https://ieeexplore.ieee.org/stamp/stamp.jsp?tp=&arnumber=4722212
+* Idea:
+  * Vertex clustering simplification algorithm
---
+++ src/simplifier.rs
@@ -0,0 +1,20 @@
+use crate::graphics::{Material, Mesh, Vertex, VertexIndices};
+use glam::{Vec3, Vec4};
+
+pub fn vertex_cluster(mesh: &Mesh) -> Mesh {
+    let mut simplified_mesh = Mesh::new(mesh.material, mesh.no_shade);
+    simplified_mesh.default_color = mesh.default_color;
+
+    // TODO: Implement vertex cluster simplification algorithm
+
+    simplified_mesh
+}
+
+pub fn qem(mesh: &Mesh) -> Mesh {
+    let mut simplified_mesh = Mesh::new(mesh.material, mesh.no_shade);
+    simplified_mesh.default_color = mesh.default_color;
+
+    // TODO: Implement QEM algorithm
+
+    simplified_mesh
+}
